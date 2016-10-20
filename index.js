8b003bfa46339d7c6d110649336bf521f22ac1a3
Tanisha <tanishahampden@gmail.com>
2016-10-20 20:30:01 +0700

    initial commit

--- index.js
+++ index.js
@@ -1,12 +1,50 @@
 #!/usr/bin/env babel-node
 
+'use strict'
+
 require('./helper')
+
+let fs = require('fs')
+let express = require('express')
+//let PromiseRouter = require('express-promise-router')
+let morgan = require('morgan')
+let trycatch = require('trycatch')
+let bodyParser = require('body-parser')
+let nodeify = require('bluebird-nodeify')
+let mime = require('mime-types')
+let path = require('path')
 
 async function main() {
     // Use 'await' in here
     console.log('main()...')
+   
+    let app = express()
+    app.use(morgan('dev'))
+
     
-    // Your implementation here
+    app.get('*', async (req, res, next) => {
+		console.log(req.url)
+		let filePath = path.join(process.cwd(), req.url)
+		let stat = await fs.promise.stat(filePath)
+		
+		console.log(stat);
+		if (stat.isDirectory()) {
+			let files = await fs.promise.readdir(filePath)
+			return res.json(files)
+		}
+		let type = await mime.lookup(filePath)
+		res.setHeader('Content-Type', type)
+		
+		res.setHeader('Content-Length', stat.size)
+		fs.createReadStream(filePath).pipe(res)
+    })
+    
+    app.head('*', (req, res) => { } )
+
+    let port = 8000
+    await app.listen(port)
+    console.log(`LISTENING @ http://127.0.0.1:${port}`)
+
 }
 
 main()
